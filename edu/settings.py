3dec2de871d7397c84f2e995e1674ba5df70bc19
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-05-19 18:16:47 +0500

    Finally

--- edu/settings.py
+++ edu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-12*ej@jml4tr44ls&#k04o&@i!f5*d!^cqo@4eva$dsm4s!y5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
